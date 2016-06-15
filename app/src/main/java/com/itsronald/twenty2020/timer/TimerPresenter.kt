f86303096f5295aaf311207bee77b252bd2101b3
Ronald Martin <ronald@ronaldsmartin.com>
2016-06-15 16:51:07 -0700

    Add referrer to Help/Feedback custom tab.

--- app/src/main/java/com/itsronald/twenty2020/timer/TimerPresenter.kt
+++ app/src/main/java/com/itsronald/twenty2020/timer/TimerPresenter.kt
@@ -3,6 +3,7 @@ package com.itsronald.twenty2020.timer
 import android.app.Activity
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.support.customtabs.CustomTabsIntent
 import android.support.v4.content.ContextCompat
@@ -55,10 +56,20 @@ class TimerPresenter
 
     override fun openHelpFeedback() {
         Timber.v("Opening Help/Feedback Chrome Custom Tab.")
+
+        // Build the intent, customizing action bar color and animations.
         val context = view.context
         val customTabsIntent = CustomTabsIntent.Builder()
             .setToolbarColor(ContextCompat.getColor(context, R.color.colorPrimary))
             .build()
+
+        // Add referrer.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            customTabsIntent.intent.putExtra(Intent.EXTRA_REFERRER,
+                    Uri.parse("${Intent.URI_ANDROID_APP_SCHEME}//${context.packageName}"))
+        }
+
+        // Open the custom tab.
         if (context is Activity) {
             customTabsIntent.launchUrl(context, Uri.parse(context.getString(R.string.help_feedback_url)))
         }
