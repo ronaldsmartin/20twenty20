178e35244494d8cbf643345e3131307a61235921
Ronald Martin <ronaldsmartin@gmail.com>
2016-06-17 00:56:51 -0700

    Provide up navigation for Settings activity.

--- app/src/main/java/com/itsronald/twenty2020/settings/SettingsActivity.kt
+++ app/src/main/java/com/itsronald/twenty2020/settings/SettingsActivity.kt
@@ -6,18 +6,12 @@ import android.annotation.TargetApi
 import android.content.Context
 import android.content.Intent
 import android.content.res.Configuration
-import android.media.Ringtone
 import android.media.RingtoneManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.preference.ListPreference
-import android.preference.Preference
-import android.preference.PreferenceActivity
-import android.support.v7.app.ActionBar
-import android.preference.PreferenceFragment
-import android.preference.PreferenceManager
-import android.preference.RingtonePreference
+import android.preference.*
+import android.support.v4.app.NavUtils
 import android.text.TextUtils
 import android.view.MenuItem
 
@@ -38,6 +32,15 @@ class SettingsActivity : AppCompatPreferenceActivity() {
         super.onCreate(savedInstanceState)
         setupActionBar()
     }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean =
+        when (item?.itemId) {
+            android.R.id.home -> {
+                NavUtils.navigateUpFromSameTask(this)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
 
     /**
      * Set up the [android.app.ActionBar], if the API is available.
