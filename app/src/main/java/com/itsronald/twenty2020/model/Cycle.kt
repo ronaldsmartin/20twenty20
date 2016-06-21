3d25cadf3d79088b81c4a91327e1f52d98d17bfa
Ronald Martin <ronald@ronaldsmartin.com>
2016-06-20 19:43:36 -0700

    Encapsulate timer logic in the Cycle model object.

--- app/src/main/java/com/itsronald/twenty2020/model/Cycle.kt
+++ app/src/main/java/com/itsronald/twenty2020/model/Cycle.kt
@@ -1,7 +1,15 @@
 package com.itsronald.twenty2020.model
 
+import rx.Observable
+import rx.Subscription
+import rx.lang.kotlin.onError
+import rx.schedulers.Schedulers
+import rx.subjects.PublishSubject
+import timber.log.Timber
+import java.util.concurrent.TimeUnit
+
 /**
- * A value representing the repeating work and break cycle in the app.
+ * Encapsulates the state of the repeating work and break cycle.
  */
 class Cycle {
 
@@ -18,22 +26,152 @@ class Cycle {
          */
         BREAK;
 
-        /**
-         * The default duration for this phase.
-         */
+        /** The total duration of this phase, in seconds. **/
+        val duration: Int
+            get() = defaultDuration
+
+        /** The default duration for this phase. */
         val defaultDuration: Int
             get() = when(this) {
                 WORK  -> 60 // 60 seconds
                 BREAK -> 30 // 30 seconds
             }
 
-        /**
-         * The next sequential phase that follows this phase.
-         */
+        /** The next sequential phase that follows this phase. */
         val nextPhase: Phase
             get() = when(this) {
                 WORK  -> BREAK
                 BREAK -> WORK
             }
+    }
+
+    /** The current phase of the cycle. **/
+    var phase = Phase.WORK
+        private set
+
+    /** Whether or not the cycle timer is currently running. */
+    var running = false
+        private set
+
+    /** The time that has elapsed current phase, in seconds. **/
+    var elapsedTime = 0
+        private set
+
+    /** The total duration of the current phase, in seconds. **/
+    val duration: Int
+        get() = phase.duration
+
+    /** The time remaining in the current phase, in seconds. **/
+    val remainingTime: Int
+        get() = duration - elapsedTime
+
+    /** PublishSubject where we update the timer state. **/
+    private val timerSubject = PublishSubject.create<Cycle>().toSerialized()
+
+    /** Observable timer for the current phase. */
+    private var countdown: Subscription? = null
+
+    /** Observable state of the cycle. */
+    val timer = timerSubject.asObservable()
+
+    //region Convenience properties
+
+    /** The total number of minutes in the duration of the current phase. **/
+    val durationMinutes: Int
+        get() = duration / 60
+
+    /** The number of minutes that have elapsed in the current phase. **/
+    val elapsedTimeMinutes: Int
+        get() = elapsedTime / 60
+
+    /** The time left in the current phase, formatted as HH:mm:ss */
+    val remainingTimeText: String
+        get() {
+            val secondsLeft = remainingTime % 60
+            val minutesLeft = (remainingTime / 60).toInt() % 60
+            val hoursLeft   = (remainingTime / (60 * 60)).toInt()
+            return when {
+                hoursLeft > 0   -> {
+                    val minutes = "$minutesLeft".padStart(2, padChar = '0')
+                    val seconds = "$secondsLeft".padStart(2, padChar = '0')
+                    "$hoursLeft:$minutes:$seconds"
+                }
+                minutesLeft > 0 -> {
+                    val seconds = "$secondsLeft".padStart(2, padChar = '0')
+                    "$minutesLeft:$seconds"
+                }
+                else            -> "$secondsLeft"
+            }
+        }
+
+
+    //endregion
+
+    /**
+     * Start the countdown for the current phase. If the phase countdown is already running, then
+     * this command will be ignored.
+     *
+     * When the phase completes, the next phase will start automatically.
+     *
+     * @param delay If specified, the phase will wait [delay] seconds before starting.
+     */
+    fun start(delay: Int = 0) {
+        if (running) {
+            Timber.i("Cycle.start() ignored: the cycle is already running.")
+            return
+        }
+        Timber.v("Starting ${phase.name} phase. Time elapsed: $elapsedTime; Time left: $remainingTime")
+
+        countdown = Observable.interval(1, TimeUnit.SECONDS)
+                .take(remainingTime)
+                .delay(delay.toLong(), TimeUnit.SECONDS)
+                .map { it.toInt() }
+                .serialize()
+                .subscribeOn(Schedulers.computation())
+                .doOnError { timerSubject.onError(it) }
+                .doOnCompleted {
+                    running = !running
+                    startNextPhase()
+                }
+                .subscribe {
+                    elapsedTime += 1
+                    if (timerSubject.hasObservers()) {
+                        timerSubject.onNext(this)
+                    }
+                }
+
+        running = true
+    }
+
+    /**
+     * Pause the countdown for the phase in progress. If the phase countdown is not running, then
+     * this command will be ignored.
+     */
+    fun pause() {
+        if (!running) {
+            Timber.i("Cycle.pause() ignored: the cycle is already paused.")
+            return
+        }
+        Timber.v("Pausing ${phase.name} phase. Time elapsed: $elapsedTime; Time left: $remainingTime")
+        countdown?.unsubscribe()
+        running = false
+    }
+
+    /**
+     * Restart the current phase.
+     */
+    fun restartPhase() {
+        elapsedTime = 0
+    }
+
+    /**
+     * Start the next phase.
+     */
+    fun startNextPhase(delay: Int = 0) {
+        countdown?.unsubscribe()
+        running     = false
+        phase       = phase.nextPhase
+        elapsedTime = 0
+        start(delay)
     }
 }
--- app/src/main/java/com/itsronald/twenty2020/timer/TimerPresenter.kt
+++ app/src/main/java/com/itsronald/twenty2020/timer/TimerPresenter.kt
@@ -21,133 +21,70 @@ import javax.inject.Inject
 class TimerPresenter
     @Inject constructor(override var view: TimerContract.TimerView)
     : TimerContract.UserActionsListener {
+    
+    val cycle = Cycle()
 
-    /// Time that the long (work) cycle should take, in seconds.
-    private val WORK_CYCLE_TIME  = Cycle.Phase.WORK.defaultDuration
-    /// Time that the short (break) cycle should take, in seconds.
-    private val BREAK_CYCLE_TIME = Cycle.Phase.BREAK.defaultDuration
+    override val running: Boolean
+        get() = cycle.running
 
-    private var currentPhase = Cycle.Phase.WORK
-
-    private val currentCycleName: String
-        get() = if (currentPhase == Cycle.Phase.WORK) "work" else "break"
-
-    override var running = false
-
-    private var timeRemaining = WORK_CYCLE_TIME
-
-    private var timeElapsed = 0
-
-    private var secondsTimer = createSecondsTimer()
-
-    private var timeLeftSubscription: Subscription? = null
-
-    private var timerStringSubscription: Subscription? = null
-
-    private var timerProgressMajorSubscription: Subscription? = null
-
-    private fun createSecondsTimer(): Observable<Int> =
-            Observable.interval(1, TimeUnit.SECONDS).take(timeRemaining).map { it.toInt() }
-
-    private fun updateTime(): Observable<Int> = secondsTimer
-            .subscribeOn(Schedulers.computation())
-            .observeOn(AndroidSchedulers.mainThread())
-            .doOnError { Timber.e(it, "Unable to update time left.") }
-            .doOnNext { timeElapsed = it }
-            .doOnCompleted {
-                Timber.i("${currentCycleName.toUpperCase()} cycle complete.")
-                running = !running
-                NotificationHelper(view.context).notifyPhaseComplete(currentPhase)
-                startNextCycle()
-            }
-
-    private fun updateTimeText(): Observable<String> = secondsTimer
-            .map { formatTimeRemaining(timeRemaining - it) }
+    private val timeStringUpdater = cycle.timer
+            .map { it.remainingTimeText }
             .subscribeOn(Schedulers.computation())
             .observeOn(AndroidSchedulers.mainThread())
             .doOnError { Timber.e(it, "Unable to update time string.") }
-            .doOnNext { view.showTimeRemaining(it) }
-            .doOnCompleted { Timber.i("timerStringSubscription finished!") }
+            .subscribe { view.showTimeRemaining(it) }
 
-    private fun updateMajorProgressBar(): Observable<Int> = secondsTimer
+    private val progressBarUpdater = cycle.timer
             .subscribeOn(Schedulers.computation())
             .observeOn(AndroidSchedulers.mainThread())
             .doOnError { Timber.e(it, "Unable to update major progress bar.") }
-            .doOnNext { elapsedSeconds ->
-                val cycleDuration        = if (currentPhase == Cycle.Phase.WORK) WORK_CYCLE_TIME else BREAK_CYCLE_TIME
-                val majorProgressMax     = (cycleDuration / 60).toInt() // Minutes in work cycle
-                val majorProgressCurrent = if (currentPhase == Cycle.Phase.WORK)
-                    majorProgressMax - (elapsedSeconds / 60).toInt() else (elapsedSeconds / 60).toInt()
-                Timber.d("Updating progress: $majorProgressCurrent / $majorProgressMax")
+            .subscribe { cycleState ->
+                val majorProgressMax     = cycleState.durationMinutes
+                val majorProgressCurrent = when (cycleState.phase) {
+                    Cycle.Phase.WORK  -> cycleState.durationMinutes - cycleState.elapsedTimeMinutes
+                    Cycle.Phase.BREAK -> cycleState.elapsedTimeMinutes
+                }
                 view.showMajorProgress(majorProgressCurrent, majorProgressMax)
             }
-            .doOnCompleted { Timber.i("Finished major progress bar update cycle.") }
 
+    private val notificationsUpdater = cycle.timer
+            .filter { it.elapsedTime == it.duration - 1 }
+            .subscribeOn(Schedulers.computation())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { NotificationHelper(view.context).notifyPhaseComplete(it.phase) }
 
     override fun onStart() {
         super.onStart()
-        view.showTimeRemaining(formatTimeRemaining(timeRemaining))
+        view.showTimeRemaining(cycle.remainingTimeText)
     }
 
-    private fun formatTimeRemaining(timeLeft: Int): String {
-        val secondsLeft = timeLeft % 60
-        val minutesLeft = (timeLeft / 60).toInt() % 60
-        val hoursLeft   = (timeLeft / (60 * 60)).toInt()
-        return when {
-            hoursLeft > 0   -> {
-                val minutes = "$minutesLeft".padStart(2, padChar = '0')
-                val seconds = "$secondsLeft".padStart(2, padChar = '0')
-                "$hoursLeft:$minutes:$seconds"
-            }
-            minutesLeft > 0 -> {
-                val seconds = "$secondsLeft".padStart(2, padChar = '0')
-                "$minutesLeft:$seconds"
-            }
-            else            -> "$secondsLeft"
-        }
-    }
-
-    private fun startNextCycle() {
-        currentPhase  = currentPhase.nextPhase
-        timeRemaining = if (currentPhase == Cycle.Phase.WORK) WORK_CYCLE_TIME else BREAK_CYCLE_TIME
-        timeElapsed   = 0
-        secondsTimer  = createSecondsTimer()
-        toggleCycleRunning()
+    override fun onStop() {
+        super.onStop()
+        timeStringUpdater.unsubscribe()
+        progressBarUpdater.unsubscribe()
+        notificationsUpdater.unsubscribe()
     }
 
     override fun toggleCycleRunning() {
-        running = !running
         if (running) {
-            startCycle()
-        } else {
             pauseCycle()
+        } else {
+            startCycle()
         }
     }
 
     private fun startCycle() {
-        Timber.v("Starting $currentCycleName cycle. Time elapsed: $timeElapsed; Time left: $timeRemaining")
-        timeLeftSubscription           = updateTime().subscribe()
-        timerStringSubscription        = updateTimeText().subscribe()
-        timerProgressMajorSubscription = updateMajorProgressBar().subscribe()
-
+        cycle.start()
         view.setFABDrawable(android.R.drawable.ic_media_pause)
     }
 
     private fun pauseCycle() {
-        timeLeftSubscription?.unsubscribe()
-        timerStringSubscription?.unsubscribe()
-        timerProgressMajorSubscription?.unsubscribe()
-
-        timeRemaining -= timeElapsed
-        secondsTimer = createSecondsTimer()
-
+        cycle.pause()
         view.setFABDrawable(android.R.drawable.ic_media_play)
-
-        Timber.v("Pausing cycle. Time elapsed: $timeElapsed; Time left: $timeRemaining")
     }
 
     override fun delayCycle() {
-        throw UnsupportedOperationException()
+        cycle.restartPhase()
     }
 
     override fun restartCycle() {
