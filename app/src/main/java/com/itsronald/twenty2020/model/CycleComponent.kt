a7f3d459f1f7ca997e44aea6f04a00ba51f67b4e
Ronald Martin <ronald@ronaldsmartin.com>
2016-06-22 13:47:42 -0700

    Inject Cycle into CycleService when the service is created.

--- app/src/main/java/com/itsronald/twenty2020/model/CycleComponent.kt
+++ app/src/main/java/com/itsronald/twenty2020/model/CycleComponent.kt
@@ -1,9 +1,11 @@
 package com.itsronald.twenty2020.model
 
+import com.itsronald.twenty2020.notifications.CycleService
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
 @Component(modules = arrayOf(CycleModule::class))
 interface CycleComponent {
+    fun inject(cycleService: CycleService)
 }
--- app/src/main/java/com/itsronald/twenty2020/notifications/CycleService.kt
+++ app/src/main/java/com/itsronald/twenty2020/notifications/CycleService.kt
@@ -3,9 +3,21 @@ package com.itsronald.twenty2020.notifications
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import com.itsronald.twenty2020.model.Cycle
+import com.itsronald.twenty2020.model.CycleModule
+import com.itsronald.twenty2020.model.DaggerCycleComponent
+import javax.inject.Inject
 
 
 class CycleService : Service() {
+
+    @Inject
+    lateinit var cycle: Cycle
+
+    override fun onCreate() {
+        super.onCreate()
+        DaggerCycleComponent.builder().cycleModule(CycleModule()).build().inject(this)
+    }
 
     override fun onBind(intent: Intent?): IBinder? {
         return null
