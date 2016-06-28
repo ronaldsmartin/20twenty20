de629a12e66b8f73cd11b33d6c2884307d55c1fe
Ronald Martin <ronald@ronaldsmartin.com>
2016-06-28 14:18:32 -0700

    Define main user preferences in XML.

--- app/src/main/java/com/itsronald/twenty2020/settings/SettingsContract.kt
+++ app/src/main/java/com/itsronald/twenty2020/settings/SettingsContract.kt
@@ -6,30 +6,6 @@ import com.itsronald.twenty2020.base.View
 
 interface SettingsContract {
 
-    companion object {
-        val KEY_WORK_PHASE_LENGTH = "com.itsronald.settings.key.work_phase_length"
-
-        val KEY_BREAK_PHASE_LENGTH = "com.itsronald.settings.key.break_phase_length"
-
-        val KEY_NIGHT_MODE = "com.itsronald.settings.key.night_mode"
-
-        val KEY_NIGHT_MODE_USES_LOCATION = "com.itsronald.settings.key.night_mode_uses_location"
-
-        val KEY_TIMER_KEEPS_SCREEN_ON = "com.itsronald.settings.key.timer_keeps_screen_on"
-
-        val KEY_TIMER_ALLOWS_FULLSCREEN = "com.itsronald.settings.key.timer_allows_fullscreen"
-
-        val KEY_NOTIFICATION_IS_PERSISTENT = "com.itsronald.settings.key.notification_is_persistent"
-
-        val KEY_NOTIFICATION_SOUND_ENABLED = "com.itsronald.settings.key.notification_sound_enabled"
-
-        val KEY_NOTIFICATION_VIBRATE_ENABLED = "com.itsronald.settings.key.notification_vibrate_enabled"
-
-        val KEY_NOTIFICATION_LED_ENABLED = "com.itsronald.settings.key.notification_led_enabled"
-
-        val KEY_NOTIFICATION_LED_COLOR = "com.itsronald.settings.key.notification_led_color"
-    }
-
     interface SettingsView : View<UserActionsListener> {
 
     }
