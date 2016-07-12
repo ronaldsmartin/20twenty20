ec797d73bdb6df828601439e93cfbb55efb87fe0
Ronald Martin <ronald@ronaldsmartin.com>
2016-07-12 13:11:36 -0700

    Configure manifest to use pre-Marshmallow custom backup agent.

---
+++ app/src/main/java/com/itsronald/twenty2020/backup/Twenty2020BackupAgent.kt
@@ -0,0 +1,12 @@
+package com.itsronald.twenty2020.backup
+
+import android.app.backup.BackupAgentHelper
+
+/**
+ * A custom agent to handle Android backup of the app's SharedPreferences.
+ *
+ * See https://developer.android.com/training/backup/backupapi.html
+ */
+class Twenty2020BackupAgent : BackupAgentHelper() {
+
+}
