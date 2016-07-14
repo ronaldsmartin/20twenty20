8e438a45a93bfff434fe7ed7beaeca714e4795a4
Ronald Martin <ronald@ronaldsmartin.com>
2016-07-13 18:22:48 -0700

    Provide minimal home screen widget through the application.

---
+++ app/src/main/java/com/itsronald/twenty2020/widget/Twenty2020WidgetProvider.kt
@@ -0,0 +1,15 @@
+package com.itsronald.twenty2020.widget
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+
+/**
+ * Provides functionality for our app widget.
+ */
+class Twenty2020WidgetProvider : AppWidgetProvider() {
+
+    override fun onUpdate(context: Context?, appWidgetManager: AppWidgetManager?, appWidgetIds: IntArray?) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+    }
+}
