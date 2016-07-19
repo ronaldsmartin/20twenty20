57e7bd0f1591c5f8206e3b3f6fb8f94d43b47f62
Ronald Martin <ronald@ronaldsmartin.com>
2016-07-19 16:34:18 -0700

    Add unit tests for Cycle.Phase.localizedName

--- app/src/test/java/com/itsronald/twenty2020/model/CyclePhaseTest.kt
+++ app/src/test/java/com/itsronald/twenty2020/model/CyclePhaseTest.kt
@@ -1,12 +1,26 @@
 package com.itsronald.twenty2020.model
 
+import com.itsronald.twenty2020.R
+import com.itsronald.twenty2020.data.ResourceRepository
 import org.junit.Test
 
 import org.junit.Assert.*
 import org.hamcrest.CoreMatchers.*
+import org.junit.Rule
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnit
+import org.mockito.junit.MockitoRule
+import org.mockito.Mockito.*
 
 
 class CyclePhaseTest {
+
+    @Mock
+    lateinit var resources: ResourceRepository
+
+    @Rule @JvmField
+    val mockRule: MockitoRule = MockitoJUnit.rule()
+
     @Test
     fun duration() {
 
@@ -25,7 +39,11 @@ class CyclePhaseTest {
 
     @Test
     fun localizedName() {
+        Cycle.Phase.WORK.localizedName(resources)
+        verify(resources).getString(R.string.phase_name_work)
 
+        Cycle.Phase.BREAK.localizedName(resources)
+        verify(resources).getString(R.string.phase_name_break)
     }
 
 }
