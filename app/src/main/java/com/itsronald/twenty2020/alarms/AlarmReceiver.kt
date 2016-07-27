f8360c5e4111170bc6d845b7d76968812a4000ff
Ronald Martin <ronald@ronaldsmartin.com>
2016-07-27 14:49:34 -0700

    Implement logic for scheduling and receiving cycle alarms.

--- app/src/main/java/com/itsronald/twenty2020/alarms/AlarmReceiver.kt
+++ app/src/main/java/com/itsronald/twenty2020/alarms/AlarmReceiver.kt
@@ -3,6 +3,8 @@ package com.itsronald.twenty2020.alarms
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import com.itsronald.twenty2020.Twenty2020Application
+import com.itsronald.twenty2020.model.Cycle
 import timber.log.Timber
 
 /**
@@ -11,9 +13,21 @@ import timber.log.Timber
 class AlarmReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
-        // TODO: Implement
-        Timber.e("Unimplemented")
-        throw UnsupportedOperationException("Unimplemented")
+        Timber.i("Received broadcast: $intent")
+
+        val appComponent = (context.applicationContext as? Twenty2020Application)?.appComponent
+
+        val completedPhase = intent.getSerializableExtra(AlarmScheduler.EXTRA_PHASE) as Cycle.Phase
+        appComponent?.notifier()?.notifyPhaseComplete(completedPhase)
+
+        val cycle = appComponent?.cycle()
+        if (cycle?.phase == completedPhase) {
+            Timber.i("Cycle is out of sync. Starting next phase.")
+            cycle?.startNextPhase()
+        }
+
+        Timber.i("Updating alarms.")
+        appComponent?.alarmScheduler()?.updateAlarms()
     }
 
 }
--- app/src/main/java/com/itsronald/twenty2020/alarms/AlarmScheduler.kt
+++ app/src/main/java/com/itsronald/twenty2020/alarms/AlarmScheduler.kt
@@ -1,7 +1,11 @@
 package com.itsronald.twenty2020.alarms
 
 import android.app.AlarmManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.SystemClock
 import com.itsronald.twenty2020.model.Cycle
 import com.itsronald.twenty2020.notifications.Notifier
 import timber.log.Timber
@@ -18,6 +22,8 @@ class AlarmScheduler
 
     companion object {
         const val REQUEST_CODE_NOTIFY_PHASE_COMPLETE = 10
+
+        const val EXTRA_PHASE = "com.itsronald.alarms.extra.cycle_phase"
     }
 
     //region lifecycle
@@ -32,4 +38,43 @@ class AlarmScheduler
 
     //endregion
 
+    fun updateAlarms() =
+        if (cycle.running) scheduleNextNotification(cycle) else cancelNextNotification(cycle)
+
+    /**
+     * Calculate the system time at which a cycle's current phase will expire.
+     *
+     * @param cycle The cycle for which to calculate the expiration time.
+     * @return the system elapsed real time at which [cycle]'s phase will expire, in milliseconds.
+     */
+    private fun phaseExpirationTime(cycle: Cycle): Long =
+            SystemClock.elapsedRealtime() + cycle.remainingTimeMillis
+
+    private fun scheduleNextNotification(cycle: Cycle) {
+        val nextNotificationTime = phaseExpirationTime(cycle)
+        Timber.i("Scheduling notification phase ${cycle.phaseName} at time $nextNotificationTime")
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            alarmManager.setExactAndAllowWhileIdle(AlarmManager.ELAPSED_REALTIME_WAKEUP, nextNotificationTime, alarmIntent(cycle))
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            alarmManager.setExact(AlarmManager.ELAPSED_REALTIME_WAKEUP, nextNotificationTime, alarmIntent(cycle))
+        } else {
+            alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, nextNotificationTime, alarmIntent(cycle))
+        }
+    }
+
+    private fun cancelNextNotification(cycle: Cycle) {
+        Timber.i("Cancelling notification for phase ${cycle.phaseName}.")
+        alarmManager.cancel(alarmIntent(cycle = cycle))
+    }
+
+    private fun alarmIntent(cycle: Cycle): PendingIntent = PendingIntent.getBroadcast(
+            context,
+            REQUEST_CODE_NOTIFY_PHASE_COMPLETE,
+            broadcastIntent(cycle = cycle),
+            PendingIntent.FLAG_UPDATE_CURRENT
+    )
+
+    private fun broadcastIntent(cycle: Cycle): Intent = Intent(context, AlarmReceiver::class.java)
+            .putExtra(EXTRA_PHASE, cycle.phase)
 }
