aede7856684c474d3c890a8e19319c16cfec3891
Ronald Martin <ronald@ronaldsmartin.com>
2016-08-16 17:52:42 -0700

    Require email address when submitting guest feedback.

--- app/src/main/java/com/itsronald/twenty2020/feedback/FeedbackActivity.kt
+++ app/src/main/java/com/itsronald/twenty2020/feedback/FeedbackActivity.kt
@@ -1,5 +1,6 @@
 package com.itsronald.twenty2020.feedback
 
+import android.os.Bundle
 import com.heinrichreimersoftware.androidissuereporter.IssueReporterActivity
 import com.heinrichreimersoftware.androidissuereporter.model.github.GithubTarget
 import com.itsronald.twenty2020.BuildConfig
@@ -12,6 +13,11 @@ class FeedbackActivity : IssueReporterActivity() {
     companion object {
         const val TARGET_USERNAME = "ronaldsmartin"
         const val TARGET_REPOSITORY = "20twenty20"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setGuestEmailRequired(true)
     }
 
     override fun getTarget(): GithubTarget = GithubTarget(TARGET_USERNAME, TARGET_REPOSITORY)
