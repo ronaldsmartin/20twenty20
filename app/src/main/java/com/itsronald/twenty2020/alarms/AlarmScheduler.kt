01458c090e9b12eb03e4cf105a23a38c4154be51
Ronald Martin <ronald@ronaldsmartin.com>
2016-08-31 14:28:42 -0700

    Restore AlarmManager's deleted timer observer.

--- app/src/main/java/com/itsronald/twenty2020/alarms/AlarmScheduler.kt
+++ app/src/main/java/com/itsronald/twenty2020/alarms/AlarmScheduler.kt
@@ -8,8 +8,11 @@ import android.content.Intent
 import android.os.Build
 import android.os.SystemClock
 import com.itsronald.twenty2020.model.Cycle
+import com.itsronald.twenty2020.model.TimerControl
 import com.itsronald.twenty2020.timer.TimerActivity
 import rx.Subscription
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
 import timber.log.Timber
 import java.util.Date
 import javax.inject.Inject
@@ -47,6 +50,23 @@ class AlarmScheduler
     init {
         Timber.i("Scheduler created.")
     }
+
+    /**
+     * Observe changes to the cycle's timer state.
+     */
+    @Suppress("unused")
+    private val timerEventSubscription = cycle.timerEvents()
+            .subscribeOn(Schedulers.computation())
+            .observeOn(AndroidSchedulers.mainThread())
+            .onErrorResumeNext {
+                Timber.e(it, "Encountered an error while observing TimerControl events.")
+                cycle.timerEvents()
+            }
+            .subscribe { event ->
+                Timber.i("Received timer event: ${TimerControl.eventName(event)}")
+                updateAlarms()
+            }
+
 
     //region Intents
 
